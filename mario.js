5d8a98dbd669ce35495cf3f16306c22ff12fe8dd
AleksandraSolomina <71250590+AleksandraSolomina@users.noreply.github.com>
2020-09-16 09:34:25 +0500

    Add files via upload

--- mario.js
+++ mario.js
@@ -1,35 +1,38 @@
 
-printPyramid(5);
+
+const DEFAULT_PYRAMID_HEIGHT = 5;
+const DEFAULT_PYRAMID_SYMBOL = "#";
+
+const symbolEl = document.getElementById("brick_type");
+const heightEl = document.getElementById("myrange");
+const numEl = document.getElementById("numb");
 
 
-// function printPyramid(height) {
-//     var elem;
+function pyramidRow(row, height, sym = DEFAULT_PYRAMID_SYMBOL) {
+    const para = document.createElement("p");
+    para.appendChild(document.createTextNode("\xa0".repeat(height - row) + sym.repeat(row + 1)));
+    return para;
+}
+
+function printPyramid(height, symbol) {
+    document.getElementById("thePyramid").innerHTML = "";
+    const pyramidContainer = document.createElement("div");
     
-  
-//     for (var i = 0; i < height; i++) {
-//         if(height-i-2>=0){
-//             res = " ".repeat(height-i-2) + "#".repeat(i + 2);
-//             // elem = document.createElement("p");
-//             // elem.innerHTML = res;
-//             // document.getElementById("pyramid").appendChild(elem);
-//     }
-//         else{
-//             res = "#".repeat(i + 2);
-//             // elem = document.createElement("p");
-//             // elem.innerHTML = res;
-//             // document.getElementById("pyramid").appendChild(elem);
-
-//         }
-
-        function printPyramid(height) {
-            for (let i = 2; i < height + 2; ++i) {
-                res = (`${' '.repeat(height + 1 - i)}${'&#9746'.repeat(i)}`);
+    for (let i = 1; i <= height; ++i)
+    pyramidContainer.appendChild(pyramidRow(i, height, symbol));
+    document.getElementById("thePyramid").appendChild(pyramidContainer);
+}
+heightEl.setAttribute("value", DEFAULT_PYRAMID_HEIGHT);
+numEl.textContent = heightEl.value;
+        
+        heightEl.addEventListener("change", e => {
             
-                elem = document.createElement("p");
-                elem.innerHTML = res;
-                document.getElementById("pyramid").appendChild(elem); 
-            }
-        }
-        document.getElementById("construction").remove(); 
-    
+            printPyramid(heightEl.value, symbolEl.value);
+            numEl.textContent = heightEl.value;
+          });      
+
+          symbolEl.addEventListener("change", e => {
+            
+            printPyramid(heightEl.value, symbolEl.value);
+          });      
 
